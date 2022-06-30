06bd464cbb58cf2b912cf3c2f8d932920e1929cc
DESKTOP-ABLVTRB\FADHIL <muhammad1900016050@webmail.uad.ac.id>
2022-06-30 12:12:38 +0700

    add assignment 8

---
+++ Sesi 23/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Route from "./Route";
+
+export default function App() {
+  return <Route />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
